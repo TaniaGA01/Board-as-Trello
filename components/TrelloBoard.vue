1af0501c16df80ef15379f69458cdc7ed1ec7265
TaniaGA01 <tanygaitan@gmail.com>
2023-02-18 12:18:55 +0100

    first commit

---
+++ components/TrelloBoard.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Column } from '~~/types';
+import { nanoid} from "nanoid"
+const columns = ref<Column[]>([
+    {
+        id: nanoid(),
+        title: " Create Marketing LandingPage",
+        tasks:[]
+    }
+])
+</script>
+<template></template>
